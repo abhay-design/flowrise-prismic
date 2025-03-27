0ca1c937d0bf0afc4edef7dd15ace450e83b2c5c
Abhay <abhay@cleardigital.com>
2025-03-27 17:33:37 +0530

    added banner

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,18 @@
 import { type Metadata } from "next";
 import { notFound } from "next/navigation";
 import { asImageSrc } from "@prismicio/client";
-// import { SliceZone } from "@prismicio/react";
+import { SliceZone } from "@prismicio/react";
 
 import { createClient } from "@/prismicio";
-// import { components } from "@/slices";
+import { components } from "@/slices";
 
 export default async function Page() {
   const client = createClient();
   const page = await client.getSingle("homepage").catch(() => notFound());
 
-  return <h1 className="text-red">{page.data.title}</h1>;
+  // return <h1 className="text-red">{page.data.title}</h1>;
 
-  // return <SliceZone slices={page.data.slices} components={components} />;
+  return <SliceZone slices={page.data.slices} components={components} />;
 }
 
 export async function generateMetadata(): Promise<Metadata> {
