e545aa7c76f8fbc1f47db0d7a7eaed11a53d24b7
Abhay <abhay@cleardigital.com>
2025-03-27 13:35:39 +0530

    added homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,29 @@
-export default function Home() {
-  return <h1 className="font-display">hello next js</h1>;
+import { type Metadata } from "next";
+import { notFound } from "next/navigation";
+import { asImageSrc } from "@prismicio/client";
+// import { SliceZone } from "@prismicio/react";
+
+import { createClient } from "@/prismicio";
+// import { components } from "@/slices";
+
+export default async function Page() {
+  const client = createClient();
+  const page = await client.getSingle("homepage").catch(() => notFound());
+
+  return <h1 className="text-red">{page.data.title}</h1>;
+
+  // return <SliceZone slices={page.data.slices} components={components} />;
+}
+
+export async function generateMetadata(): Promise<Metadata> {
+  const client = createClient();
+  const page = await client.getSingle("homepage").catch(() => notFound());
+
+  return {
+    title: page.data.meta_title,
+    description: page.data.meta_description,
+    openGraph: {
+      images: [{ url: asImageSrc(page.data.meta_image) ?? "" }],
+    },
+  };
 }
